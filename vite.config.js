7278a89124868c60730aa56197f23475802e3d13
Zane Davis <zaneadavis19@gmail.com>
2026-04-20 16:01:12 -0400

    Update Hardcoded Names

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nc-county-financials/',
+  base: '/nc-county-financial-explorer/',
   build: {
     emptyOutDir: false,
   }
